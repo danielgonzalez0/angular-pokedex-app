331fdd44f7f577b0b01e900497657ea82691a754
Daniel Gonzalez <daniel.gonzalez.02@gmail.com>
2024-10-30 17:30:58 +0100

    feature: add signal

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,11 +14,11 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'angular-pokedex-app' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-pokedex-app');
-  });
+  // it(`should have the 'angular-pokedex-app' title`, () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   const app = fixture.componentInstance;
+  //   expect(app.title).toEqual('angular-pokedex-app');
+  // });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular-pokedex-app';
+  name = signal<string>('Pikachu');
+  life = signal<number>(21);
+  
+  size = computed((): string=>{
+ switch (true) {
+   case this.life() < 15:
+     return 'Petit';
+   case this.life() > 25:
+     return 'Grand';
+   default:
+     return 'Moyen';
+ }
+  })
+
+  incrementLife(): void {
+    this.life.update((n) => n + 1);
+    console.log('+1 point de vie');
+  }
+
+  decrementeLife(): void {
+    this.life.update((n) => n - 1);
+    console.log('-1 point de vie');
+  }
+
 }
